703f3a475d0722c03b0089cb1afc730a94a555ba
Philippe Marguinaud <pmarguinaud@hotmail.com>
2021-03-03 15:51:36 +0100

    First commit

---
+++ n.F90
@@ -0,0 +1,15 @@
+SUBMODULE (m) n                   ! The descendant submodule n
+  INTEGER :: j                    ! Specification part
+
+  CONTAINS                        ! Module subprogram part
+    MODULE SUBROUTINE sub1(arg1)  ! Definition of sub1 by subroutine subprogram
+      INTEGER :: arg 1
+      arg1 = 1
+      i = 2                       ! Host association
+      j = 3                       ! Host association
+    END SUBROUTINE
+
+    MODULE PROCEDURE sub2         ! Definition of sub2 by separate module subprogram
+      arg2 = 1
+    END PROCEDURE
+END SUBMODULE
