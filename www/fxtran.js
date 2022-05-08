4ea9314809ade90b655597d8dbfa50f1865677d0
Philippe Marguinaud <pmarguinaud@hotmail.com>
2022-05-08 18:00:01 +0200

    Highlight variables with click selection

--- www/fxtran.js
+++ www/fxtran.js
@@ -1,8 +1,16 @@
 var fxtranURI = 'http://fxtran.net/#syntax';
+var htmlURI = 'http://www.w3.org/1999/xhtml';
+
+function parseHTML (html)
+{
+  var doc = document.implementation.createHTMLDocument ('');
+  doc.documentElement.innerHTML = html;
+  return doc.body.firstChild;
+}
 
 function nsResolver (prefix) 
 {
-  const ns = {'f': fxtranURI};
+  const ns = {'f': fxtranURI, 'html': htmlURI};
   return ns[prefix] || null;
 }
  
@@ -11,9 +19,38 @@ function ucFirst (s)
   return s.charAt (0).toUpperCase () + s.slice (1);
 }
 
-function NClick ()
+function NClick (n)
 {
-  console.log ('NClick');
+  let t = n.textContent;
+  let ns = document.evaluate ('//f:N[string(.)="' + t + '"]', 
+                               document, nsResolver, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+  console.log (n);
+  console.log (n.parentNode);
+
+  if (n.parentNode.namespaceURI == htmlURI)
+    {
+      
+      for (let i = 0; i < ns.snapshotLength; i++)
+        {
+          let s = ns.snapshotItem (i);
+          if (s.parentNode.namespaceURI == htmlURI)
+            {
+              s.parentNode.replaceWith (s);
+            }
+        }
+    }
+  else
+    {
+      let TT = parseHTML ('<tt style="background-color:red;"/>');
+
+      for (let i = 0; i < ns.snapshotLength; i++)
+        {
+          let s = ns.snapshotItem (i);
+          let t = TT.cloneNode (true);
+          t.appendChild (s.cloneNode (true));
+          s.replaceWith (t);
+        }
+    }
 }
 
 function methodExists (method)
@@ -29,6 +66,8 @@ function methodCall (method, n)
 function _onclick (e)
 {
   let n = e.target;
+
+  console.log ("_onclick = ", e);
 
   while (n && (n.namespaceURI == fxtranURI))
     {
@@ -52,8 +91,6 @@ function _onclick (e)
 
 function _onload ()
 {
-
-  
   let ns;
   ns = document.evaluate ('//f:file', document, nsResolver, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
   
@@ -104,8 +141,8 @@ function _onload ()
   for (let i = 0; i < ns.snapshotLength; i++)
     {
       let x = ns.snapshotItem (i);
-      console.log (x);
-      x.addEventListener ('click', function (e) { console.log (e); console.log ("CALL"); x.remove ();  });
+//    console.log (x);
+//    x.addEventListener ('click', function (e) { console.log (e); console.log ("CALL"); x.remove ();  });
     }
 
   document.addEventListener ('click', _onclick);
