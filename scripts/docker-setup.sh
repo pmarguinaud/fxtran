980c4de07fc67b6ae6d1aee85c3ceea026278f36
Philippe Marguinaud <pmarguinaud@hotmail.com>
2023-07-09 17:48:43 +0200

    Build for Debian unstable

--- scripts/docker-setup.sh
+++ scripts/docker-setup.sh
@@ -4,39 +4,103 @@
 set -x
 set -e
 
-if [ 1 -eq 1 ]
-then
-sudo docker pull ubuntu:latest
+unset LANG
+unset LC_ALL
+unset LANGUAGE
 
-cat > fxtran.sh << EOF
+EMAIL=pmarguinaud@hotmail.com
+NAME="Philippe Marguinaud"
+DEBIAN=debian:unstable
+
+sudo docker pull $DEBIAN
+
+if [ ! -f "$HOME/.ssh/known_hosts" ]
+then
+  touch "$HOME/.ssh/known_hosts" 
+fi
+
+if [ ! -f "$HOME/.ssh/id_rsa" ]
+then
+  ssh-keygen -b 2048 -t rsa -f "$HOME/.ssh/id_rsa" -q -N ""
+fi
+
+SSHKEY=$(cat $HOME/.ssh/id_rsa.pub)
+SHARED=$PWD/../$DEBIAN
+TIMEZONE=$(cat /etc/timezone)
+
+\rm -rf $SHARED
+mkdir -p $SHARED
+
+mkdir -p $SHARED/.ssh
+cat > $SHARED/.ssh/authorized_keys << EOF
+$SSHKEY
+EOF
+
+chmod 600 $SHARED/.ssh/authorized_keys
+
+cat -> $SHARED/.bash_profile << EOF
+
+export EMAIL=$EMAIL
+unset LANG
+
+set -o vi
+
+EOF
+
+cat -> $SHARED/.vimrc << EOF
+:set mouse=
+:set hlsearch
+EOF
+
+cat > $SHARED/fxtran-boot.sh << EOF
 #!/bin/bash
 
 set -x
 set -e
 
+export DEBIAN_FRONTEND=noninteractive
+
 dir=\$(dirname \$0)
 
 cd \$dir
 
+ln -snf /usr/share/zoneinfo/$TIMEZONE /etc/localtime 
+echo $TIMEZONE > /etc/timezone
+
 apt-get -y update 
 apt-get -y dist-upgrade
+apt-get -y install git vim ssh screen sudo
+apt-get -y install debhelper-compat
+apt-get -y install g++ make build-essential debhelper \
+  python3-dev python3-setuptools dh-exec python3-pip devscripts lintian
 
-apt-get -y install ./fxtran_1-0_amd64.deb
-apt-get -y install ./libfxtran-perl_1-0_amd64.deb
-apt-get -y install ./python-fxtran_1-0_amd64.deb
+echo 'X11UseLocalhost no' >> /etc/ssh/sshd_config
 
-/usr/bin/fxtran -help
-perl -e ' use fxtran;  print &fxtran::run ("END\n") ' 
-python2.7 -c 'import fxtran;  print (fxtran.run ("END\n")) '
+service ssh start
+
+useradd $USER
+chsh -s /bin/bash $USER
+
+cat >> /etc/sudoers << EOC
+
+$USER ALL=(root) NOPASSWD: /usr/bin/dpkg, NOPASSWD: /usr/bin/apt, NOPASSWD: /usr/bin/apt-get, NOPASSWD: /usr/bin/dpkg, NOPASSWD: /bin/su
+EOC
 
 EOF
 
-chmod +x fxtran.sh
+chmod 755 $SHARED/fxtran-boot.sh 
 
-sudo docker run -t -d --name ubuntu_fxtran \
-  --mount type=bind,src=$PWD/,dst=/root/fxtran,readonly=true \
-  ubuntu:latest
+sudo docker \
+  run -h fxtran_build -t -d \
+  --name fxtran_build \
+  --mount type=bind,src=$SHARED/,dst=/home/$USER \
+  --mount type=bind,src=$PWD/,dst=/home/$USER/fxtran \
+  $DEBIAN
 
-sudo docker exec ubuntu_fxtran /root/fxtran/fxtran.sh
+sudo docker exec fxtran_build /home/$USER/fxtran-boot.sh 
 
-fi
+IP=$(sudo docker inspect -f "{{ .NetworkSettings.IPAddress }}" fxtran_build)
+ssh-keygen -f "$HOME/.ssh/known_hosts" -R ${IP}
+
+echo "${IP}" > fxtran_build.txt
+
